bbf1438b4248709eba811446bfd5a49648998155
Pedro <pedro.alvarez.v17@gmail.com>
2023-03-30 09:37:08 +0200

    Constructor creado

---
+++ Ejercicio_GIT_Eclipse/src/Alumno.java
@@ -0,0 +1,12 @@
+
+public class Alumno {
+	private String nombre, apellido, dni;
+
+	public Alumno(String nombre, String apellido, String dni) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	} 
+	
+	
+}
