9a57b9848bf6c85f12c17a164f7118a98798f16c
Pedro <pedro.alvarez.v17@gmail.com>
2023-03-30 09:45:42 +0200

    Alumno creado e impreso

--- Ejercicio_GIT_Eclipse/src/principal.java
+++ Ejercicio_GIT_Eclipse/src/principal.java
@@ -4,6 +4,8 @@ public class principal {
 	public static void main(String[] args) {
 		System.out.println("Listado de alumnos 1º DAW");
 		System.out.println("Alumno 1:");
+		Alumno pako = new Alumno("Pako","Francisco","01234567A");
+		System.out.println(pako);
 	}
 
 }
